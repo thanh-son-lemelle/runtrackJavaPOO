b6cc9de33af278463403a75f064443997d54bcd1
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-07-01 10:53:05 +0200

    depot job 02&04

---
+++ jour06/job02/Main.java
@@ -0,0 +1,13 @@
+package jour06.job02;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("bonjour " + name);
+
+        scanner.close();
+    }
+}
---
+++ jour06/job03/Main.java
@@ -0,0 +1,13 @@
+package jour06.job03;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] myArray = {0, 1, 2, 3, 4, 5, 6 ,7, 8, 9};
+        
+        System.out.println(myArray[0]);
+        System.out.println(myArray[1]);
+        System.out.println(myArray[5]);
+        System.out.println(myArray[9]);
+        System.out.println(myArray[10]);
+    }
+}
